225de6deeb444792381b72cc39ff051689818330
MarcinMuss <marcinmusialik16@gmail.com>
2021-05-20 21:13:10 +0200

    Persisting the changes in the API and client

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -36,5 +37,20 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;   //username z tokenu, ktorego API uzywa do
+            var user = await _userRepository.GetUserByUsernameAsync(username); //uwierzytelnienia tego uzytkownika
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }                                                                        
     }
 }
