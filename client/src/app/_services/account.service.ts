87b05915943b62846c68a0df093bbf04eb505287
MarcinMuss <marcinmusialik16@gmail.com>
2021-06-10 09:26:08 +0200

    Setting the main photo in the API and adding to the nav bar

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {  //sa niszczone gdy nie sa uzywane (taka roznica)
       map((response: User) => {                                        //przed zasubskrybowaniem ich
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -31,14 +30,14 @@ export class AccountService {  //sa niszczone gdy nie sa uzywane (taka roznica)
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
