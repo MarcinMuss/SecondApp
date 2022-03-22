a981bf5bb6a2dc244d017aa983bde7594d789d46
MarcinMuss <marcinmusialik16@gmail.com>
2022-03-22 20:32:44 +0100

    refactoring to asp.net core identity

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,33 +1,27 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return; //sprawdzenie czy sa jacys uzytkownicy, jesli nie kod idzie dalej
+            if (await userManager.Users.AnyAsync()) return; //sprawdzenie czy sa jacys uzytkownicy, jesli nie kod idzie dalej
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData); //zwykla lista users typu AppUsers
+            if (users == null) return;
+
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
-
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password")); // takie samo haslo dla kazdego usera
-                user.PasswordSalt = hmac.Key;
-                
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "password");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
